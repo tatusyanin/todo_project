9c27b3047d90002bb6a4ac6d989f37be07c3018a
溝渕正利 <jo-jaku@ip-10-95-3-50.ap-northeast-1.compute.internal>
2024-07-03 11:07:45 +0900

    todo_list.html

---
+++ todo/admin 2.py
@@ -0,0 +1,6 @@
+# todo/admin.py
+
+from django.contrib import admin
+from .models import TodoItem
+
+admin.site.register(TodoItem)
--- todo_project/settings.py
+++ todo_project/settings.py
@@ -129,9 +129,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
