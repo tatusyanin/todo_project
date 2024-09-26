f1989a3690d0c9d8e7920c924a46e058886e034e
溝渕正利 <jo-jaku@M1.local>
2024-09-26 15:32:53 +0900

    css change

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v790sf&hsbnuy0seqpxo501b2vd5=1(e0$&dkagn^o7$f7=kni'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['todolist-masatoshimizofuchi-b.onrender.com', 'localhost', '127.0.0.1']
 USE_X_FORWARDED_HOST = True
